8f06d96414760cd3bcb45750c9c403700ea12c07
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-26 20:10:12 +0530

    hardhatconfig.js and scripts/deploy.js modified for ganache

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
-  defaultNetwork: "sepolia",
   networks: {
-    hardhat: {},
-    sepolia: {
-      url: `https://rpc-sepolia.sepolia.io`,
-      accounts: [`0x${process.env.SE_POLIA_PRIVATE_KEY}`],
-    },
-  },
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200,
+    ganache: {
+      url: "HTTP://127.0.0.1:7545",
+      accounts: [
+        "0x649ff52003eba23cfbe700f6c2a07c68b2a78d5faa98a92866ac6fc202ad5aab",
+        "0x57aec85fb245400998dc896c65b2e7459a91812bb0f1051902d2901b3abd4941",
+      ],
     },
   },
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const balance = await deployer.getBalance();
+  console.log("Account balance:", balance.toString());
+
+  const Wallet = await ethers.getContractFactory("Wallet");
+  const wallet = await Wallet.deploy();
+
+  console.log("Wallet contract deployed to:", wallet.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
