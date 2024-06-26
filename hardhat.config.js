aa98a50a64d64cc0aefb5056154b7e121b420940
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-26 19:51:05 +0530

    some cleanup done

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,18 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "sepolia",
   networks: {
+    hardhat: {},
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      url: `https://rpc-sepolia.sepolia.io`,
+      accounts: [`0x${process.env.SE_POLIA_PRIVATE_KEY}`],
+    },
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
     },
   },
 };
